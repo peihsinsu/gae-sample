0b846b1aaccdca2c85f341f652574677a7508262
peihsinsu <simonsu.mail@gmail.com>
2013-09-09 00:42:10 +0800

    add memcache sample

--- src/com/gcp/gae/TestWebServlet.java
+++ src/com/gcp/gae/TestWebServlet.java
@@ -1,12 +1,15 @@
 package com.gcp.gae;
 
 import java.io.IOException;
-import java.util.Date;
+import java.util.logging.Level;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.appengine.api.memcache.ErrorHandlers;
+import com.google.appengine.api.memcache.MemcacheService;
+import com.google.appengine.api.memcache.MemcacheServiceFactory;
 import com.google.appengine.api.taskqueue.Queue;
 import com.google.appengine.api.taskqueue.QueueFactory;
 import com.google.appengine.api.taskqueue.TaskOptions;
@@ -27,7 +30,7 @@ public class TestWebServlet extends HttpServlet {
 			throws IOException {
 		UserService userService = UserServiceFactory.getUserService();
 	    User usr = userService.getCurrentUser();
-		String user = ""; 
+		String user = "no user from getUserPrincipal"; 
 		if(req.getUserPrincipal()!=null) 
 			user = req.getUserPrincipal().getName();
 		
@@ -36,7 +39,7 @@ public class TestWebServlet extends HttpServlet {
 		if(act.equalsIgnoreCase("post")) {
 			System.out.println("Putting Post Queue Job.....");
 			Queue q = QueueFactory.getDefaultQueue();
-			q.add(TaskOptions.Builder.withUrl("/Test").param("id", "12341234").method(Method.PULL));//.method(Method.GET));
+			q.add(TaskOptions.Builder.withUrl("/Test").param("id", "12341234").method(Method.GET));
 		} else if(act.equalsIgnoreCase("pull")){
 			System.out.println("Putting Post Queue Job.....");
 			Queue q = QueueFactory.getQueue("ProcessedOrderQueue");
@@ -44,8 +47,16 @@ public class TestWebServlet extends HttpServlet {
 		}
 		
 		
+		MemcacheService mem = MemcacheServiceFactory.getMemcacheService();
+		mem.setErrorHandler(ErrorHandlers.getLogAndContinue(Level.INFO));
+		System.out.println("put key to memcache....");
+		mem.put("key", "value");
+		Object key = mem.get("key");
+		System.out.println("Get key from memcache... " + key.toString());
+		
+		
 		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello...." + usr.getNickname() + "::" + user  );
+		resp.getWriter().println("Hello...." + (usr!=null?usr.getNickname():"no usr from getCurrentUser") + "::" + user  );
 	}
 	
 	public void doPull(HttpServletRequest req, HttpServletResponse resp)
